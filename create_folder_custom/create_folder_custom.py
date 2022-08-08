828397a0b8a489b5b057e51a7cb70de8bbc32706
René Frassek <rene.frassek@getpioneers.com>
2022-08-08 19:38:38 +0200

    Added switch & description for custom text Todo: functionality

--- create_folder_custom/create_folder_custom.py
+++ create_folder_custom/create_folder_custom.py
@@ -13,53 +13,53 @@ ui = ap.UI() # setup UI
 def calculate_dateandtime(dialog):
     folder_time = str(datetime.now().strftime("%Y-%m-%d_%H-%M"))
 
-    parentDirectory = apc.path
+    parent_directory = apc.path
 
-    doProjectName = dialog.get_value("doProjectName")
+    # grab dialog switch variable "include project name"
+    do_project_name = dialog.get_value("do_project_name")
 
-    if doProjectName:
+    if do_project_name:
         projectName = pathlib.Path(apc.project_path)
-        print(doProjectName)
-        directoryToCreate = parentDirectory + "/" + projectName.name + "_" + folder_time
-        create_folder(directoryToCreate, dialog)
+        directory_to_create = parent_directory + "/" + projectName.name + "_" + folder_time
+        create_folder(directory_to_create, dialog)
     else:
-        directoryToCreate = parentDirectory + "/" + folder_time
-        create_folder(directoryToCreate, dialog)
+        directory_to_create = parent_directory + "/" + folder_time
+        create_folder(directory_to_create, dialog)
 
 
 # DATE (YYYY-MM-DD) based folder path calculation for folder creation
 def calculate_date(dialog):
     folder_time = str(datetime.now().strftime("%Y-%m-%d"))
     
-    parentDirectory = apc.path
+    parent_directory = apc.path
 
-    doProjectName = dialog.get_value("doProjectName")
+    # grab dialog switch variable "include project name"
+    do_project_name = dialog.get_value("do_project_name")
 
-    if doProjectName:
+    if do_project_name:
         projectName = pathlib.Path(apc.project_path)
-        print(doProjectName)
-        directoryToCreate = parentDirectory + "/" + projectName.name + "_" + folder_time
-        create_folder(directoryToCreate, dialog)
+        directory_to_create = parent_directory + "/" + projectName.name + "_" + folder_time
+        create_folder(directory_to_create, dialog)
     else:
-        directoryToCreate = parentDirectory + "/" + folder_time
-        create_folder(directoryToCreate, dialog)
+        directory_to_create = parent_directory + "/" + folder_time
+        create_folder(directory_to_create, dialog)
     
 
 # create folders
-def create_folder(directoryToCreate, dialog):
+def create_folder(directory_to_create, dialog):
 
-    print(directoryToCreate)
+    print(directory_to_create)
     
     mode = 0o666
     exist_ok = True
-    os.makedirs(directoryToCreate, mode, exist_ok)
-    print("Directory " + directoryToCreate + " created")
+    os.makedirs(directory_to_create, mode, exist_ok)
+    print("Directory " + directory_to_create + " created")
     
     dialog.close()
     ui.show_info("Folder successfully created.", "", 2500)
-    
 
 
+# Build GUI
 dialog = ap.Dialog()
 
 dialog.title ="Create custom folder"
@@ -67,8 +67,11 @@ dialog.add_text("<b>What kind of folder do you want to create?</b>")
 dialog.add_button("Date", callback = calculate_date).add_info("YYYY-MM-DD")
 dialog.add_button("Date and Time", callback = calculate_dateandtime).add_info("YYYY-MM-DD_HH-MM")
 dialog.add_empty()
-dialog.add_switch(False, var = "doProjectName").add_text("Include project name")
+dialog.add_switch(False, var = "do_project_name").add_text("Include project name")
 dialog.add_info("Enable to include the project name in the folder name.")
+# dialog.add_switch(False, var = "do_custom_text").add_text("Include custom text")
+# dialog.add_info("Enable to include custom text in the folder name.")
+# dialog.add_text("Custom text: ").add_input("", var = "custom_text")
 
 dialog.show()
 
