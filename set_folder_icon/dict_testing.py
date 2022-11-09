c8c5720bc2af00c4856e024ba86b34a787314ad5
René Frassek <rene.frassek@getpioneers.com>
2022-11-09 14:43:37 +0100

    added file with struct for adding of icon and color functionality

---
+++ set_folder_icon/dict_testing.py
@@ -0,0 +1,163 @@
+dirsToCreate = [
+    ############## Beratung
+    {
+        "path" : "Beratung/Konzept", 
+        "icon" : "eye",
+        "color" : "#ff0000"
+    },
+    ############## Kundendaten
+    {
+        "path" : "Kundendaten", 
+        "icon" : "eye",
+        "color" : ""
+    },
+    ############## Produktion / Applikationen / 360_2D_Events
+    {
+        "path" : "Produktion/Applikationen/360_2D_Events/3DVista", 
+        "icon" : "eye",
+        "color" : ""
+    },
+    {
+        "path" : "Produktion/Applikationen/360_2D_Events/krpano", 
+        "icon" : "eye",
+        "color" : ""
+    },
+    {
+        "path" : "Produktion/Applikationen/360_2D_Events/PTGui", 
+        "icon" : "eye",
+        "color" : ""
+    },
+    ############## Produktion / Applikationen / Output
+    {
+        "path" : "Produktion/Applikationen/Output/WebGL",
+        "icon" : "eye",
+        "color" : ""
+    },
+    ############## Produktion / Applikationen / Realtime
+    {
+        "path" : "Produktion/Applikationen/Realtime/ThreeJS",
+        "icon" : "eye",
+        "color" : ""
+    },
+    {
+        "path" : "Produktion/Applikationen/Realtime/UEngine",
+        "icon" : "eye",
+        "color" : ""
+    },
+    {
+        "path" : "Produktion/Applikationen/Realtime/Unity",
+        "icon" : "eye",
+        "color" : ""
+    },
+    ############## Produktion / Applikationen / Website_HTML / Bootstrap
+    {
+        "path" : "Produktion/Applikationen/Website_HTML/Bootstrap/HTML",
+        "icon" : "eye",
+        "color" : ""
+    },
+    ############## Produktion / CGI
+    {
+        "path" : "Produktion/CGI/previews",
+        "icon" : "eye",
+        "color" : ""
+    },
+    {
+        "path" : "Produktion/CGI/renderoutput",
+        "icon" : "eye",
+        "color" : ""
+    },
+    {
+        "path" : "Produktion/CGI/renderpresets",
+        "icon" : "eye",
+        "color" : ""
+    },
+    {
+        "path" : "Produktion/CGI/sceneassets",
+        "icon" : "eye",
+        "color" : ""
+    },
+    {
+        "path" : "Produktion/CGI/scenes/xref/objects",
+        "icon" : "eye",
+        "color" : ""
+    },
+    ############## Produktion / Grafik-Design
+    {
+        "path" : "Produktion/Grafik-Design/Edit",
+        "icon" : "eye",
+        "color" : ""
+    },
+    {
+        "path" : "Produktion/Grafik-Design/Footage",
+        "icon" : "eye",
+        "color" : ""
+    },
+    {
+        "path" : "Produktion/Grafik-Design/Output",
+        "icon" : "eye",
+        "color" : ""
+    },
+    ############## Produktion / Postproduktion
+    {
+        "path" : "Produktion/Postproduktion/Footage",
+        "icon" : "eye",
+        "color" : ""
+    },
+    {
+        "path" : "Produktion/Postproduktion/Output/AS1-Final",
+        "icon" : "eye",
+        "color" : ""
+    },
+    {
+        "path" : "Produktion/Postproduktion/Output/Final",
+        "icon" : "eye",
+        "color" : ""
+    },
+    {
+        "path" : "Produktion/Postproduktion/Output/Prefinal",
+        "icon" : "eye",
+        "color" : ""
+    },
+    {
+        "path" : "Produktion/Postproduktion/Output/Preview",
+        "icon" : "eye",
+        "color" : ""
+    },
+    {
+        "path" : "Produktion/Postproduktion/Videoedit",
+        "icon" : "eye",
+        "color" : ""
+    },
+    ############## Vorproduktion
+    {
+        "path" : "Vorproduktion/Drehbuch",
+        "icon" : "eye",
+        "color" : ""
+    },
+    {
+        "path" : "Vorproduktion/Inspiration",
+        "icon" : "eye",
+        "color" : ""
+    },
+]
+
+i = 0
+while i < (len(dirsToCreate)):
+    print("index: " + str(i))
+    print(dirsToCreate[i]["path"])
+    print(dirsToCreate[i]["icon"])
+    print(dirsToCreate[i]["color"])
+    print("--------------------------------------------------")
+    i += 1
+
+
+
+
+
+
+
+
+
+
+
+
