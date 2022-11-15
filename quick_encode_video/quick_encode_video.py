9465bb421eedbfafba90124cb703c2c78e91b1e8
René Frassek <rene.frassek@getpioneers.com>
2022-11-15 17:17:35 +0100

    fixed ui bug
    Former-commit-id: 6a4bb4a52b4e00b80c3909ce85e4531249d675d5

--- quick_encode_video/quick_encode_video.py
+++ quick_encode_video/quick_encode_video.py
@@ -5,21 +5,25 @@ import string
 import anchorpoint
 import apsync
 
-apc = anchorpoint.Context.instance() # setup anchorpoint context
-ui = anchorpoint.UI() # setup UI
+apc = anchorpoint.Context.instance()  # setup anchorpoint context
+ui = anchorpoint.UI()  # setup UI
 
 projectPath = str(pathlib.Path(apc.project_path))
 folderPath = apc.folder
 filePath = apc.path
 fileName = apc.filename.rstrip(string.digits).rstrip("-,.")
 
-execPath = os.path.abspath(os.path.dirname(__file__)) # get python script folder path for ffmpeg
+# get python script folder path for ffmpeg
+execPath = os.path.abspath(os.path.dirname(__file__))
 
 # --------------------------------------
 # UI Buttons, Dropdowns, etc. functions
 # --------------------------------------
+
+
 def button_test(value):
     pass
+
 
 def dropdown_quality(dialog, value):
     dialog.hide_row("input_customQuality", value != "Custom")
@@ -27,22 +31,27 @@ def dropdown_quality(dialog, value):
     dialog.hide_row("info_quality", value == "Custom")
     check_button_encode()
 
+
 def input_customQuality(dialog):
     pass
+
 
 def switch_resolution(dialog, value):
     dialog.hide_row("dropdown_resolution", value == True)
     check_button_encode()
 
+
 def dropdown_resolution(dialog, value):
     check_button_encode()
 
 # function for enablding / disabling encode button
+
+
 def check_button_encode():
-    if (dialog.get_value("dropdown_quality")) != "Select Quality": # is quality set
-        if (dialog.get_value("switch_resolution")) == True: # is switch enabled
+    if (dialog.get_value("dropdown_quality")) != "Select Quality":  # is quality set
+        if (dialog.get_value("switch_resolution")) == True:  # is switch enabled
             dialog.set_enabled("button_encode", True)
-        elif (dialog.get_value("dropdown_resolution")) != "Select Resolution": # is resolution set
+        elif (dialog.get_value("dropdown_resolution")) != "Select Resolution":  # is resolution set
             dialog.set_enabled("button_encode", True)
         else:
             dialog.set_enabled("button_encode", False)
@@ -50,6 +59,8 @@ def check_button_encode():
 # ----------------------
 # Function for encoding
 # ----------------------
+
+
 def button_encode(dialog):
     # get all relevant values from the interface
     quality = dialog.get_value("dropdown_quality")
@@ -70,10 +81,12 @@ def button_encode(dialog):
     else:
         encodingQuality = customQuality
 
-    outputPath = folderPath + "/" + fileName + "_QuickEncode" + ".mp4" # build path for new file
+    outputPath = folderPath + "/" + fileName + \
+        "_QuickEncode" + ".mp4"  # build path for new file
 
     # encode video
-    os.system(execPath + "\\ffmpeg.exe -i " + filePath + " -c:v libx264 -crf " + str(encodingQuality) + " -preset medium -b:v 0 " + outputPath)
+    os.system(execPath + "\\ffmpeg.exe -i " + filePath + " -c:v libx264 -crf " +
+              str(encodingQuality) + " -preset medium -b:v 0 " + outputPath)
 
     # close dialog, inform user and output some debug info
     dialog.close()
@@ -86,26 +99,34 @@ def button_encode(dialog):
 # ---------------------------------
 # Function for closing the dialoge
 # ---------------------------------
+
+
 def button_close(dialog: anchorpoint.Dialog):
     dialog.close()
 
 
 # --------------
-# Dialog Window 
+# Dialog Window
 # --------------
 dropdownQualityValues = ["Very High", "High", "Medium", "Low", "Custom"]
 dropdownResolutionValues = ["75%", "50%", "25%"]
 
 dialog = anchorpoint.Dialog()
 
-dialog.title ="Quick Encode Video"
-dialog.add_text("Quality: ").add_dropdown("Select Quality", dropdownQualityValues, callback = dropdown_quality, var = "dropdown_quality")
-dialog.add_text("Custom CRF:").add_input("30", callback = input_crf, var = "input_customQuality")
-dialog.add_info("Default Quality settings are set with CRF values as follows:<br>Very High: 17 | High: 20 | Medium: 25 | Low: 35", var = "info_quality")
-dialog.add_info("The range of the CRF scale is 0-51, where 0 is lossless.<br>Sane values are 17-28. ~17 is visually nearly lossless.", var = "info_quality_crf")
-dialog.add_switch(True, callback = switch_resolution, var = "switch_resolution").add_text("Keep Resolution")
-dialog.add_text("Resolution: ").add_dropdown("Select Resolution", dropdownResolutionValues, callback = dropdown_resolution, var = "dropdown_resolution")
-dialog.add_button("Encode", callback = button_encode, var = "button_encode", enabled = False)
+dialog.title = "Quick Encode Video"
+dialog.add_text("Quality: ").add_dropdown("Select Quality",
+                                          dropdownQualityValues, callback=dropdown_quality, var="dropdown_quality")
+dialog.add_text("Custom CRF:").add_input(
+    "30", callback=input_customQuality, var="input_customQuality")
+dialog.add_info(
+    "Default Quality settings are set with CRF values as follows:<br>Very High: 17 | High: 20 | Medium: 25 | Low: 35", var="info_quality")
+dialog.add_info("The range of the CRF scale is 0-51, where 0 is lossless.<br>Sane values are 17-28. ~17 is visually nearly lossless.", var="info_quality_crf")
+dialog.add_switch(True, callback=switch_resolution,
+                  var="switch_resolution").add_text("Keep Resolution")
+dialog.add_text("Resolution: ").add_dropdown("Select Resolution",
+                                             dropdownResolutionValues, callback=dropdown_resolution, var="dropdown_resolution")
+dialog.add_button("Encode", callback=button_encode,
+                  var="button_encode", enabled=False)
 
 # TODO: Implement Resizing
 # TODO: Implement CRF Value Checking
@@ -114,5 +135,3 @@ dialog.show()
 dialog.hide_row("input_customQuality")
 dialog.hide_row("info_quality_crf")
 dialog.hide_row("dropdown_resolution")
-
-
