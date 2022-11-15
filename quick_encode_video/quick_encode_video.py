ccb23c82f00866eba57693020824952646fc37d6
René Frassek <rene.frassek@getpioneers.com>
2022-11-15 16:00:46 +0100

    set up quick_encode_video ui

---
+++ quick_encode_video/quick_encode_video.py
@@ -0,0 +1,102 @@
+from typing_extensions import Self
+import os
+import pathlib
+import anchorpoint
+import apsync
+
+apc = anchorpoint.Context.instance() # setup anchorpoint context
+ui = anchorpoint.UI() # setup UI
+
+projectPath = str(pathlib.Path(apc.project_path))
+
+# --------------------------------------
+# UI Buttons, Dropdowns, etc. functions
+# --------------------------------------
+def button_test(value):
+    pass
+
+def dropdown_quality(dialog, value):
+    dialog.hide_row("input_customQuality", value != "Custom")
+    dialog.hide_row("info_quality_crf", value != "Custom")
+    dialog.hide_row("info_quality", value == "Custom")
+    check_button_encode()
+
+def input_customQuality(dialog):
+    pass
+
+def switch_resolution(dialog, value):
+    dialog.hide_row("dropdown_resolution", value == True)
+    check_button_encode()
+
+def dropdown_resolution(dialog, value):
+    check_button_encode()
+
+# function for enablding / disabling encode button
+def check_button_encode():
+    if (dialog.get_value("dropdown_quality")) != "Select Quality": # is quality set
+        if (dialog.get_value("switch_resolution")) == True: # is switch enabled
+            dialog.set_enabled("button_encode", True)
+        elif (dialog.get_value("dropdown_resolution")) != "Select Resolution": # is resolution set
+            dialog.set_enabled("button_encode", True)
+        else:
+            dialog.set_enabled("button_encode", False)
+
+# ----------------------
+# Function for encoding
+# ----------------------
+def button_encode(dialog):
+    # get all relevant values from the interface
+    quality = dialog.get_value("dropdown_quality")
+    customQuality = dialog.get_value("input_customQuality")
+    resolution = dialog.get_value("dropdown_resolution")
+
+    # set up encoding quality
+    encodingQuality = None
+    if quality != "Custom":
+        if quality == "Very High":
+            encodingQuality = 17
+        elif quality == "High":
+            encodingQuality = 20
+        elif quality == "Medium":
+            encodingQuality = 25
+        elif quality == "Low":
+            encodingQuality = 35
+    else:
+        encodingQuality = customQuality
+    
+    print(encodingQuality)
+    
+    # encode video
+    # os.system("ffmpeg -i " + source_file + " -c:v libx265 -crf " + str(crf_value) + "  -preset slower -tag:v hvc1 -b:v 0 " + export_file)
+
+# ---------------------------------
+# Function for closing the dialoge
+# ---------------------------------
+def button_close(dialog: anchorpoint.Dialog):
+    dialog.close()
+
+# --------------
+# Dialog Window 
+# --------------
+
+dropdownQualityValues = ["Very High", "High", "Medium", "Low", "Custom"]
+dropdownResolutionValues = ["75%", "50%", "25%"]
+
+dialog = anchorpoint.Dialog()
+
+dialog.title ="Quick Encode Video"
+dialog.add_text("Quality: ").add_dropdown("Select Quality", dropdownQualityValues, callback = dropdown_quality, var = "dropdown_quality")
+dialog.add_text("Custom CRF:").add_input("30", callback = input_crf, var = "input_customQuality")
+dialog.add_info("Default Quality settings are set with CRF values as follows:<br>Very High: 17 | High: 20 | Medium: 25 | Low: 35", var = "info_quality")
+dialog.add_info("The range of the CRF scale is 0-51, where 0 is lossless.<br>Sane values are 17-28. ~17 is visually nearly lossless.", var = "info_quality_crf")
+dialog.add_switch(True, callback = switch_resolution, var = "switch_resolution").add_text("Keep Resolution")
+dialog.add_text("Resolution: ").add_dropdown("Select Resolution", dropdownResolutionValues, callback = dropdown_resolution, var = "dropdown_resolution")
+dialog.add_button("Encode", callback = button_encode, var = "button_encode", enabled = False)
+# dialog.add_button("TEST", callback = button_test)
+
+dialog.show()
+dialog.hide_row("input_customQuality")
+dialog.hide_row("info_quality_crf")
+dialog.hide_row("dropdown_resolution")
+
+
