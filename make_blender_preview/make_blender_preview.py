694f1683f5d8eecb7eeb8d1bdecbeeaa40085551
René Frassek <rene.frassek@getpioneers.com>
2023-01-26 13:46:28 +0100

    implemented exception handling when no files already exist
    Former-commit-id: a109fa081302ac13f45f716e943a17d51b2411a4

--- make_blender_preview/make_blender_preview.py
+++ make_blender_preview/make_blender_preview.py
@@ -9,20 +9,30 @@ import os
 ui = ap.UI()
 ctx = ap.Context.instance()
 
+
 def create_random_text():
-    ran = "".join(random.choices(string.ascii_uppercase + string.digits, k=10))    
+    ran = "".join(random.choices(string.ascii_uppercase + string.digits, k=10))
     return str(ran)
 
+
 def render_blender(blender_path, selected_files, yaml_dir):
-    
+
     # show progress
-    progress = ap.Progress("Blender Preview", "Rendering Images", infinite = True, cancelable = len(selected_files) > 1)
+    progress = ap.Progress(
+        "Blender Preview",
+        "Rendering Images",
+        infinite=True,
+        cancelable=len(selected_files) > 1,
+    )
 
     for file in selected_files:
-        output = (file[:file.rfind(".")])
-        
+        output = file[: file.rfind(".")]
+
         # delete old files if they exist
-        os.remove((file[:file.rfind(".")]) + ".jpg")
+        try:
+            os.remove((file[: file.rfind(".")]) + ".jpg")
+        except:
+            print("no existing files detected, skipping deletion")
 
         if progress.canceled:
             for file in ctx.selected_files:
@@ -32,20 +42,29 @@ def render_blender(blender_path, selected_files, yaml_dir):
         subprocess.run(
             [
                 blender_path,
-                "-b", file,
-                "-E", "BLENDER_EEVEE",
-                "-F", "JPEG",
-                "-P", f"{yaml_dir}/make_blender_preview_eevee_settings.py",
-                "-o", f"{output}#",
-                "-f", "0",
+                "-b",
+                file,
+                "-E",
+                "BLENDER_EEVEE",
+                "-F",
+                "JPEG",
+                "-P",
+                f"{yaml_dir}/make_blender_preview_eevee_settings.py",
+                "-o",
+                f"{output}#",
+                "-f",
+                "0",
             ]
         )
 
     # rename files after render due to blender command line always inserting frame number into the file name
     for file in selected_files:
-        os.rename((file[:file.rfind(".")]) + "0" + ".jpg", (file[:file.rfind(".")]) + ".jpg")
+        os.rename(
+            (file[: file.rfind(".")]) + "0" + ".jpg", (file[: file.rfind(".")]) + ".jpg"
+        )
 
     ui.show_success("Renders Successful")
+
 
 # First, check if the tool can be found on the machine
 if "blender" in ctx.inputs:
@@ -54,11 +73,13 @@ if "blender" in ctx.inputs:
     if blender_path.lower().endswith("blender.app"):
         blender_path = os.path.join(blender_path, "Contents/MacOS/Blender")
 
-    if ap.check_application(blender_path, f"Path to Blender is not correct, please try again", "blender"):
+    if ap.check_application(
+        blender_path, f"Path to Blender is not correct, please try again", "blender"
+    ):
         # Tell the UI that these files are being processed
         for file in ctx.selected_files:
             ui.show_busy(file)
-        
+
         # Render the thumbnail
         # We don't want to block the Anchorpoint UI, hence we run on a background thread
         ctx.run_async(render_blender, blender_path, ctx.selected_files, ctx.yaml_dir)
